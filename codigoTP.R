1fc016b32bdf4ea80d98d55d2d3bed2df81763a9
FrancoCae <francocaetano20@gmail.com>
2025-11-29 18:23:27 -0300

    guardo cambios locales

--- codigoTP.R
+++ codigoTP.R
@@ -32,6 +32,8 @@ for (j in seq_along(ns)){
 
 cubrimientoEmpirico <- data.frame(n = ns, cubrimiento = cubrimientoN)
 
+#### 2.1.6 ####
+
 # Parámetros
 
 Se <- 0.9
@@ -60,4 +62,127 @@ lines(ns, ECM_perfecto, col="blue", lwd=2)
 legend("topright",
        legend=c("ECM test imperfecto", "ECM test perfecto"),
        col=c("orange", "blue"), lwd=2)
+
+
+#### 2.1.7 ####
+
+
+ns <- c(30, 50, 100, 200, 500, 1000)   # tamaños muestrales
+R  <- 10000                            # número de simulaciones
+
+### Función para el estimador de momentos
+tita_momentos <- function(p_hat, Se, Sp){
+  (p_hat - (1 - Sp)) / (Se + Sp - 1)
+}
+
+### TABLA PARA GUARDAR RESULTADOS
+
+
+resultados <- data.frame(
+  n = ns,
+  sesg_perf = NA,
+  var_perf  = NA,
+  ecm_perf  = NA,
+  sesg_imp  = NA,
+  var_imp   = NA,
+  ecm_imp   = NA
+)
+
+### SIMULACIONES
+
+
+for (i in 1:length(ns)) {
+  
+  n <- ns[i]
+  
+  # vectores para guardar simulaciones
+  est_perf <- numeric(R)
+  est_imp  <- numeric(R)
+  
+  for (r in 1:R) {
+    
+    ###Simulo Y ~ Bernoulli(theta)
+    Y <- rbinom(n, 1, tita)
+    
+    ### Simulo T dado Y
+    T <- ifelse(Y == 1, 
+                rbinom(n, 1, Se),
+                rbinom(n, 1, 1 - Sp))
+    
+    ### Estimador perfecto
+    est_perf[r] <- mean(Y)
+    
+    ### Estimador imperfecto
+    p_hat <- mean(T)
+    est_imp[r] <- tita_momentos(p_hat, Se, Sp)
+  }
+  
+  
+  # test perfecto
+  sesg_p <- mean(est_perf) - tita
+  var_p  <- var(est_perf)
+  ecm_p  <- var_p + sesg_p^2
+  
+  # test imperfecto
+  sesg_i <- mean(est_imp) - tita
+  var_i  <- var(est_imp)
+  ecm_i  <- var_i + sesg_i^2
+  
+  # guardo
+  resultados$sesg_perf[i] <- sesg_p
+  resultados$var_perf[i]  <- var_p
+  resultados$ecm_perf[i]  <- ecm_p
+  
+  resultados$sesg_imp[i] <- sesg_i
+  resultados$var_imp[i]  <- var_i
+  resultados$ecm_imp[i]  <- ecm_i
+}
+
+
+print(resultados)
+
+### GRAFICAR ECM PARA COMPARAR
+
+plot(resultados$n, resultados$ecm_perf,
+     type="b", pch=19, ylim=c(0, max(resultados$ecm_perf)),
+     xlab="n", ylab="ECM",
+     main="ECM Test Perfecto vs Test Imperfecto")
+
+lines(resultados$n, resultados$ecm_imp, type="b", pch=19, col="red")
+
+legend("topright",
+       legend=c("Perfecto", "Imperfecto"),
+       col=c("black","red"), pch=19)
+
+
+### GRAFICAR SESGO
+# Calcular límites mínimos y máximos del sesgo en ambos tests, porque si no se corta el grafico a la mitad
+lim_inf <- min(resultados$sesg_perf, resultados$sesg_imp)
+lim_sup <- max(resultados$sesg_perf, resultados$sesg_imp)
+
+# Graficar el sesgo 
+plot(resultados$n, resultados$sesg_perf,
+     type="b", pch=19,
+     xlab="n", ylab="Sesgo",
+     ylim = c(lim_inf, lim_sup),
+     main="Sesgo Perfecto vs Imperfecto")
+
+lines(resultados$n, resultados$sesg_imp,
+      type="b", pch=19, col="red")
+
+legend("topright",
+       legend=c("Perfecto", "Imperfecto"),
+       col=c("black", "red"),
+       pch=19)
+
+### GRAFICAR VARIANZA
+
+plot(resultados$n, resultados$var_perf,
+     type="b", pch=19, ylim=range(c(resultados$var_perf, resultados$var_imp)),
+     xlab="n", ylab="Varianza",
+     main="Varianza Perfecto vs Imperfecto")
+lines(resultados$n, resultados$var_imp, type="b", pch=19, col="red")
+legend("topright",
+       legend=c("Perfecto", "Imperfecto"),
+       col=c("black","red"), pch=19)
 
